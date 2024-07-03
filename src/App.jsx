a8c56738dadf60584fe9cf4d39405a51f47b3a12
Humaira Islam <ihumaira.webfront@gmail.com>
2024-07-03 20:34:07 +0600

    Form validation added

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,44 @@ function App() {
     setShowPass(!showPass)
   }
 
+  // ==========state part start
+  const [email, setEmail]=useState('')
+  const [pass, setPass]=useState('')
+  const [emailError, setEmailError]=useState('')
+  const [passError, setPassError]=useState('')
+
+  // =========state part end
+
+  // ===========function part start 
+  const handleEmail=(e)=>{
+    setEmail(e.target.value)
+    setEmailError('')
+  }
+
+  const handlePassword=(e)=>{
+    setPass(e.target.value)
+    setPassError('')
+  }
+ // ==============function part end
+
+//  =============submit part start
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+
+    if(!email){
+      setEmailError('Please enter a valid email address')
+    }
+    else if(!pass){
+      setPassError('Please enter your password')
+    }
+  
+  }
+
+// ============submit part end
+
+
+
   return (
     <>
     <div className='flex flex-col items-center justify-center h-screen'>
@@ -21,17 +59,21 @@ function App() {
         
         {
           showPass?
-          <FaEye onClick={handleShowPass} className='absolute left-[78%] bottom-[18.5%]'/>
+          <FaEye onClick={handleShowPass} className='absolute left-[78%] bottom-[17%]'/>
           :
-          <FaEyeSlash onClick={handleShowPass} className='absolute left-[78%] bottom-[18.5%]'/>
+          <FaEyeSlash onClick={handleShowPass} className='absolute left-[78%] bottom-[17%]'/>
         }   
         <h2 className='text-[40px] font-bold'>Welcome Back!</h2>
 
         <Lottie animationData={login} loop={true} />
-      
-        <input className="w-[350px] h-[40px] border-[2px] border-black p-2" type="email" placeholder='Enter your email' /><br />
-        <input className='w-[350px] h-[40px] border-[2px] border-black p-2' type={showPass? 'text':'password'} placeholder='Enter your password' />
-        <button className='mt-5 bg-black text-white font-bold p-4 rounded-lg'>Sign in</button>
+        <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center'>
+          <input onChange={handleEmail} className="w-[350px] h-[40px] border-[2px] border-black p-2" type="email" placeholder='Enter your email' />
+          <p className='text-lg font-medium text-red-600'>{emailError}</p>
+          <input onChange={handlePassword} className='w-[350px] h-[40px] border-[2px] border-black p-2 mt-[10px]' type={showPass? 'text':'password'} placeholder='Enter your password' />
+          <p className='text-lg font-medium text-red-600'>{passError}</p>
+          <button type='submit' className='mt-5 bg-black text-white font-bold p-4 rounded-lg'>Sign in</button>
+        </form>     
+   
       </div>
 
     </div>
